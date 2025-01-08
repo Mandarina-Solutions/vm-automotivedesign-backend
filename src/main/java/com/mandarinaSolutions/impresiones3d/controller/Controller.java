ae927c268a6bdb0889577a6c728ca0a04991dd48
diegolentz <diegoolentz@gmail.com>
2025-01-08 19:06:18 -0300

    lombock no funciona

--- src/main/java/com/mandarinaSolutions/impresiones3d/controller/Controller.java
+++ src/main/java/com/mandarinaSolutions/impresiones3d/controller/Controller.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mandarinaSolutions.impresiones3d.dominio.Articulo;
@@ -24,6 +26,12 @@ public class Controller {
 	@GetMapping("articulos/{id}")
 	public Articulo getArticulo(@PathVariable Integer id) {
 		return repo.findById(id).get();
+	}
+	
+	@PostMapping("crearArticulo")
+	public void addArticulo(@RequestBody Articulo articulo) {
+		System.out.println(articulo);
+		repo.save(articulo);
 	}
 
 	
--- src/main/java/com/mandarinaSolutions/impresiones3d/dominio/Articulo.java
+++ src/main/java/com/mandarinaSolutions/impresiones3d/dominio/Articulo.java
@@ -1,5 +1,7 @@
 package com.mandarinaSolutions.impresiones3d.dominio;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,28 +12,94 @@ import lombok.Data;
 
 @Entity
 @Data
-@Table(name ="articulos")
+@Table(name = "articulos")
 public class Articulo {
-	
+
+  
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	
-	@Column
-	private String descripcion;
-	
-	@Column
-	private Double precio;
-	
-	@Column
-	private String titulo;
-	
-	@Column
-	private String dimension;
-	
-	@Column
-	private String imagen;
-	
-	@Column
-	private Integer categoria;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String descripcion;
+
+    @Column
+    private Double precio;
+
+    @Column
+    private String titulo;
+
+    @Column
+    private String dimension;
+
+    @Column
+    private String imagen;
+
+    @Column
+    private Integer categoria;
+    
+    
+    public Integer getId() {
+  		return id;
+  	}
+
+  	public void setId(Integer id) {
+  		this.id = id;
+  	}
+
+  	public String getDescripcion() {
+  		return descripcion;
+  	}
+
+  	public void setDescripcion(String descripcion) {
+  		this.descripcion = descripcion;
+  	}
+
+  	public Double getPrecio() {
+  		return precio;
+  	}
+
+  	public void setPrecio(Double precio) {
+  		this.precio = precio;
+  	}
+
+  	public String getTitulo() {
+  		return titulo;
+  	}
+
+  	public void setTitulo(String titulo) {
+  		this.titulo = titulo;
+  	}
+
+  	public String getDimension() {
+  		return dimension;
+  	}
+
+  	public void setDimension(String dimension) {
+  		this.dimension = dimension;
+  	}
+
+  	public String getImagen() {
+  		return imagen;
+  	}
+
+  	public void setImagen(String imagen) {
+  		this.imagen = imagen;
+  	}
+
+  	public Integer getCategoria() {
+  		return categoria;
+  	}
+
+  	public void setCategoria(Integer categoria) {
+  		this.categoria = categoria;
+  	}
+
+  	@Override
+  	public String toString() {
+  		return "Articulo [id=" + id + ", descripcion=" + descripcion + ", precio=" + precio + ", titulo=" + titulo
+  				+ ", dimension=" + dimension + ", imagen=" + imagen + ", categoria=" + categoria + "]";
+  	}
+    
 }
